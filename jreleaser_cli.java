2d8bafa4598d6de355de7b660ffc38a4dfedab38
jreleaserbot <jreleaser@kordamp.org>
2023-10-13 16:33:31 +0200

    jreleaser-cli 23.09.3

---
+++ jreleaser_cli.java
@@ -0,0 +1,10 @@
+//usr/bin/env jbang "$0" "$@" ; exit $?
+//JAVA 11+
+//DEPS com.dotcms:jreleaser-cli:23.09.3
+// Generated with JReleaser 1.8.0 at 2023-10-13T16:33:22.611666+02:00
+
+public class jreleaser_cli {
+    public static void main(String... args) throws Exception {
+        com.dotcms.GreetingCommand.main(args);
+    }
+}
