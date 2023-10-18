b0c6c66b5bfaa1035108642be02dc6a82bc132f2
jreleaserbot <jreleaser@kordamp.org>
2023-10-18 13:11:13 +0200

    qcli-jbang early-access

--- qcli_jbang_snapshot.java
+++ qcli_jbang_snapshot.java
@@ -2,7 +2,7 @@
 //JAVA 11+
 //REPOS jitpack
 //DEPS com.github.dotCMS:qcli-jbang:main-SNAPSHOT
-// Generated with JReleaser 1.8.0 at 2023-10-18T13:04:40.099125+02:00
+// Generated with JReleaser 1.8.0 at 2023-10-18T13:11:07.277856+02:00
 
 public class qcli_jbang_snapshot {
     public static void main(String... args) throws Exception {
