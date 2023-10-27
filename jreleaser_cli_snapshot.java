3c7df217e7f89af3f5d1912061ab7935fe403e8f
jreleaserbot <jreleaser@kordamp.org>
2023-10-27 14:24:58 +0200

    jreleaser-cli 1.0.0-SNAPSHOT-SNAPSHOT

---
+++ jreleaser_cli_snapshot.java
@@ -0,0 +1,11 @@
+//usr/bin/env jbang "$0" "$@" ; exit $?
+//JAVA 11+
+//REPOS jitpack
+//DEPS com.github.dotCMS:jreleaser-cli:main-SNAPSHOT
+// Generated with JReleaser 1.8.0 at 2023-10-27T14:24:51.383144+02:00
+
+public class jreleaser_cli_snapshot {
+    public static void main(String... args) throws Exception {
+        com.dotcms.GreetingCommand.main(args);
+    }
+}
