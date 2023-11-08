374bb2f9b14303248102e742a7e608899113534e
jreleaserbot <jreleaser@kordamp.org>
2023-11-08 18:02:09 +0100

    qcli-jbang v1.1.0

--- qcli_jbang.java
+++ qcli_jbang.java
@@ -1,7 +1,7 @@
 //usr/bin/env jbang "$0" "$@" ; exit $?
 //JAVA 11+
 //DEPS com.dotcms:qcli-jbang:1.1.0
-// Generated with JReleaser 1.8.0 at 2023-10-19T21:12:17.568578+02:00
+// Generated with JReleaser 1.8.0 at 2023-11-08T18:01:51.184913+01:00
 
 public class qcli_jbang {
     public static void main(String... args) throws Exception {
