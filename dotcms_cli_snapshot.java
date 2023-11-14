8e4caf237d8eee00891348fb6a4cbe2beafe444c
Daniel Enrique Colina Rodríguez <danielcolina@gmail.com>
2023-11-14 16:23:44 +0100

    Update dotcms_cli_snapshot.java

--- dotcms_cli_snapshot.java
+++ dotcms_cli_snapshot.java
@@ -1,7 +1,8 @@
 //usr/bin/env jbang "$0" "$@" ; exit $?
 //JAVA 11+
-//REPOS jitpack
-//DEPS com.github.dotCMS.core:dotcms-cli:issue-26500-implement-configure-jbang-on-dotcms-cli-release-process-SNAPSHOT
+// //REPOS jitpack
+// //DEPS com.github.dotCMS.core:dotcms-cli:issue-26500-implement-configure-jbang-on-dotcms-cli-release-process-SNAPSHOT
+//DEPS com.dotcms:dotcms-cli:1.0.0-SNAPSHOT
 // Generated with JReleaser 1.8.0 at 2023-11-14T14:19:04.528817+01:00
 
 public class dotcms_cli_snapshot {
