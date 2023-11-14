bb263965f5b39e12adc153733ee5005aba79bc3f
jreleaserbot <jreleaser@kordamp.org>
2023-11-14 14:19:37 +0100

    dotcms-cli dotcms-cli1.0.0-SNAPSHOT-SNAPSHOT

--- dotcms_cli_snapshot.java
+++ dotcms_cli_snapshot.java
@@ -2,7 +2,7 @@
 //JAVA 11+
 //REPOS jitpack
 //DEPS com.github.dotCMS.core:dotcms-cli:issue-26500-implement-configure-jbang-on-dotcms-cli-release-process-SNAPSHOT
-// Generated with JReleaser 1.8.0 at 2023-11-13T18:03:01.960784+01:00
+// Generated with JReleaser 1.8.0 at 2023-11-14T14:19:04.528817+01:00
 
 public class dotcms_cli_snapshot {
     public static void main(String... args) throws Exception {
