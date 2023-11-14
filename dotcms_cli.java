102143daafa318acd475d560f2bfaf2ed20eae67
jreleaserbot <jreleaser@kordamp.org>
2023-11-14 13:25:53 +0100

    dotcms-cli dotcms-cli1.0.0

---
+++ dotcms_cli.java
@@ -0,0 +1,10 @@
+//usr/bin/env jbang "$0" "$@" ; exit $?
+//JAVA 11+
+//DEPS com.dotcms:dotcms-cli:1.0.0
+// Generated with JReleaser 1.8.0 at 2023-11-14T13:25:22.627783+01:00
+
+public class dotcms_cli {
+    public static void main(String... args) throws Exception {
+        com.dotcms.cli.command.EntryCommand.main(args);
+    }
+}
